f8a9f74375250b9cafab825b04819d43f43c2913
Gaurav kumar <38521462+kumargaurav05@users.noreply.github.com>
2020-09-25 18:30:52 +0530

    done

---
+++ main.js
@@ -0,0 +1,6 @@
+function on(){
+   var button = document.getElementById('ball');
+   button.classList.toggle('active');
+   var button_box = document.getElementById("btn");
+   button_box.classList.toggle("button_active");
+}
